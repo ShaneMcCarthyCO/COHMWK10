72c2782db111f9cd0a0ace03a6ed97b9445f04df
ShaneMcCarthyCO <shane.mccarthy@1888investments.com>
2020-12-06 19:35:43 -0700

    changes

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ function buildTeam() {
     if (!fs.existsSync(OUTPUT_DIR)) {
     fs.mkdirSync(OUTPUT_DIR)
   }
-  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  fs.writeFileSync(outputPath, render(Team), "utf-8");
 }
 manager()
 
